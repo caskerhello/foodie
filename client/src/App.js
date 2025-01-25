fe566643db54e5fee9cfc388af55ab8629f73521
hana <o3o_s2__@naver.com>
2025-01-26 01:14:52 +0900

    feat: 회원가입 시 최초 프로필 사진 띄우기 (표시만)

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,10 @@ import ScrollToTop from './Component/ScrollToTop';
 import Login from './Component/Login';
 import Main from './Component/Main';
 import Join from './Component/member/Join';
-import WritePost from './Component/post/WritePost';
-import PostOne from './Component/post/PostOne';
 import MyPage from './Component/member/MyPage';
 import EditProfile from './Component/member/EditProfile';
+import WritePost from './Component/post/WritePost';
+import PostOne from './Component/post/PostOne';
 import GetPlace from './Component/place/GetPlace';
 import GetCategoryPlace from './Component/place/GetCategoryPlace';
 // import Kakaosaveinfo from './Component/member/Kakaosaveinfo';
@@ -19,28 +19,18 @@ import GetCategoryPlace from './Component/place/GetCategoryPlace';
 function App() {
   return (
     <div className="App" style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
-            
-            
             <Routes>
-            
                 <Route path="/" element={<Login />} />
                 <Route path="/main" element={<Main />} />
-               
-                <Route path="/writePost" element={<WritePost />} />
-
+                <Route path="/join" element={<Join />} />
                 <Route path="/myPage" element={<MyPage />} />
-
-                <Route path="/join" element={<Join />}></Route>
-
                 <Route path="/editProfile" element={<EditProfile />} />
 
+                <Route path="/writePost" element={<WritePost />} />
                 <Route path="/postOne/:postid" element={<PostOne />} />
-
                 <Route path="/getPlace/:searchPlace" element={<GetPlace />} />
-
                 <Route path="/getCategoryPlace/:category" element={<GetCategoryPlace />} />
 
-                
                 
                 {/* <Route path="/join" element={<Join />} />
                 <Route path="/main" element={<Main />} />
@@ -52,8 +42,6 @@ function App() {
                 <Route path="/memberPage/:membernick" element={<MemberPage />} /> */}
         
             </Routes>
-           
-            
         </div>
   );
 }
